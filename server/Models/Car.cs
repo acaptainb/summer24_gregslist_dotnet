71f34e0fa0eec221c0b9148b0b92f7beaa3c01af
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-13 10:23:42 -0600

    car model

---
+++ server/Models/Car.cs
@@ -0,0 +1,18 @@
+namespace gregslist_dotnet.Models;
+
+public class Car
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public string Make { get; set; }
+  public string Model { get; set; }
+  public int Year { get; set; }
+  public int Price { get; set; }
+  public string Color { get; set; }
+  public string EngineType { get; set; }
+  public bool LeaksOil { get; set; }
+  public string Description { get; set; }
+  public string ImgUrl { get; set; }
+  public string CreatorId { get; set; }
+}
